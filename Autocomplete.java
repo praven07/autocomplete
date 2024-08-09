469200fb10b62482ff760a502f14428d83fe715a
Praven <praven2010@live.com>
2024-08-08 21:03:08 -0500

    Added boilerplate code

---
+++ Autocomplete.java
@@ -0,0 +1,22 @@
+public class Autocomplete {
+
+    // Initializes the data structure from the given array of terms.
+    public Autocomplete(Term[] terms) {
+
+    }
+
+    // Returns all terms that start with the given prefix,
+    // in descending order of weight.
+    public Term[] allMatches(String prefix) {
+
+    }
+
+    // Returns the number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix) {
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ BinarySearchDeluxe.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+
+public class BinarySearchDeluxe {
+
+    // Returns the index of the first key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+
+    }
+
+    // Returns the index of the last key in the sorted array a[]
+    // that is equal to the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
